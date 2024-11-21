8b549ecd6b7e36bcedf0a67f062244e4a95582d4
krishnaa-awasthi <2k23.csiot2312270@gmail.com>
2024-11-21 14:49:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+  display.value += number;
+}
+
+function appendOperator(operator) {
+  const lastChar = display.value.slice(-1);
+  if (['+', '-', '*', '/'].includes(lastChar)) {
+    display.value = display.value.slice(0, -1);
+  }
+  display.value += operator;
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value) || '';
+  } catch {
+    display.value = 'Error';
+  }
+}
